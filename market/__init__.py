5c08eb70a5f28d6e6292a9773055ac90f667f693
Onur Yilmaz <onuryilmaz2015@hotmail.com>
2022-02-25 12:53:00 +0300

    Register Page added

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' #app'imizin database tanıması için config
+app.config['SECRET_KEY'] = '6d44279c7aa12a146ee1af62'
 db = SQLAlchemy(app) #Python Class yapısı ile Database oluşturmak için kullanıyoruz
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')   # farklı 2 dizinde aynı sayfa
@@ -13,3 +14,9 @@ def market_page():
     items = Item.query.all()
 
     return render_template('market.html', items=items)
+
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form = form)
